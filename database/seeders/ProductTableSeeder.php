5ccc41c1ecfbde1c48f9fb6f085ffab2da85387c
AMK1215 <aungmyokhaing1215@gmail.com>
2024-10-12 12:19:38 +0630

    shan first init

---
+++ database/seeders/ProductTableSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin\Product;
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'code' => '1006',
+                'name' => 'Pragmatic Play',
+                'short_name' => 'PP',
+                'order' => 1,
+                'status' => 1
+            ],
+
+            [
+                'code' => '1085',
+                'name' => 'JDB',
+                'short_name' => 'JDB',
+                'order' => 2,
+                'status' => 1
+            ],
+
+            [
+                'code' => '1007',
+                'name' => 'PG Soft',
+                'short_name' => 'PGSoft',
+                'order' => 3,
+                'status' => 1
+            ],
+
+
+            [
+                'code' => '1091',
+                'name' => 'Jili',
+                'short_name' => 'JILI',
+                'order' => 4,
+                'status' => 1
+            ],
+
+            [
+                'code' => '1150',
+                'name' => 'Live22SM',
+                'short_name' => 'Live22',
+                'order' => 5,
+                'status' => 1
+            ],
+
+        ];
+
+        //Product::insert($data);
+        foreach ($data as $obj) {
+            Product::create($obj);
+        }
+
+    }
+}
